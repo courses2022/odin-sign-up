ad4b828891d29b5ff2459e5273f19e694dd6f618
rajpranz <rajeshwarrprasad@yahoo.com>
2022-05-31 01:13:04 +1200

    Show required messages on name email and phone

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # odin-sign-up
 Sign up form project as part of the Odin Project
+
+--------------------
+Photo by Isak Pettersson on Unsplash
--- script.js
+++ script.js
@@ -15,28 +15,37 @@ button.addEventListener("click", ()=>{
     console.log(password1.value);
     
     if(firstName.value ==""){
-        console.log("First name is required");
+        nameErrorMsg.classList.remove("hidden");
+    }else{
+        nameErrorMsg.classList.add("hidden");
     }
 
     if(email.value ==""){
         console.log("Email is required");
+        emailErrorMsg.classList.remove("hidden");
     }
     else if(email.validity.valid == false){
         console.log("Email is not valid");
+        emailErrorMsg.classList.remove("hidden");
     }
     
     if(phone.value == ""){
         console.log("phone number is required");
+        phoneErrorMsg.classList.remove("hidden");
     }
     else if(!phone.validity.valid){
         console.log("Phone number is not valid");
+        phoneErrorMsg.classList.remove("hidden");
     }
-    else if(password1.value == ""){
+
+    if(password1.value == ""){
         console.log("please enter a password");
+        passwordErrorMsg.classList.remove("hidden");
         
     }
     else if(password1.value != password2.value){
         console.log("passwords dont match");
+        passwordErrorMsg.classList.remove("hidden");
     }
     else{
         alert("Thanks we will be in touch shortly");
